96a9f53beeffa7707a06d98afaa42ac0dc24a1d3
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2023-06-04 13:49:48 -0700

    save the input points_colors in the mean camera coordinate; and save the meta data (clip_path. mean_extrinsic, min_range and max_range)

--- preprocess_track3D/preprocess_track3D.py
+++ preprocess_track3D/preprocess_track3D.py
@@ -8,6 +8,7 @@ import argparse
 import os
 import multiprocessing
 import torch
+import json
 from utils import getCamera, matrix_to_quaternion, quaternion_to_matrix
 
 DEBUG_MODE = True
@@ -43,7 +44,9 @@ def get_parser():
     return parser
 
 
-def processClip(clip_path, output_path):
+def processClip(clip_path, output_path, clip_index):
+    existDir(f"{output_path}/{clip_index}")
+
     camera_base_path = f"{clip_path}/camera"
     depth_base_path = f"{clip_path}/depth"
     rgb_base_path = f"{clip_path}/rgb"
@@ -70,7 +73,7 @@ def processClip(clip_path, output_path):
         pcd_data = pcd.rgbd2pcd(rgb_path, depth_path, camera_path)
         # Record all relevant information
         pcds.append(pcd)
-        if DEBUG_MODE and True:
+        if DEBUG_MODE and False:
             # Visualize in this frame
             fx = pcd.intrinsic[0, 0]
             fy = pcd.intrinsic[1, 1]
@@ -98,19 +101,59 @@ def processClip(clip_path, output_path):
     mean_extrinsic[0:3, 0:3] = mean_rotation_matrix
     mean_extrinsic[0:3, 3] = mean_translation
 
-    if DEBUG_MODE and True:
+    if DEBUG_MODE and False:
         # Visualize the mean camera pose
         camera = getCamera(
-                np.linalg.inv(mean_extrinsic), fx, fy, cx, cy, scale=1, z_flip=True
-            )
+            np.linalg.inv(mean_extrinsic), fx, fy, cx, cy, scale=1, z_flip=True
+        )
         coordinate = o3d.geometry.TriangleMesh.create_coordinate_frame()
         all_data.append(coordinate)
         all_data += camera
         o3d.visualization.draw_geometries(all_data)
 
-    import pdb
+    # Transform all point cloud into the average camera coordinate
+    assert len(pcds) == len(frame_names)
+    if DEBUG_MODE:
+        all_data = []
 
-    pdb.set_trace()
+    min_range = None
+    max_range = None
+
+    for i in range(len(pcds)):
+        new_pcd_data = pcds[i].pcd.transform(mean_extrinsic)
+        points = np.asarray(new_pcd_data.points)
+        colors = np.asarray(new_pcd_data.colors)
+        points_colors = np.concatenate([points, colors], axis=1)
+
+        if min_range is None:
+            min_range = np.min(points, axis=0)
+            max_range = np.max(points, axis=0)
+        else:
+            min_range = np.min([min_range, np.min(points, axis=0)], axis=0)
+            max_range = np.max([max_range, np.max(points, axis=0)], axis=0)
+
+        # Save the points_colors data
+        existDir(dir=f"{output_path}/{clip_index}/pcd")
+        np.save(f"{output_path}/{clip_index}/pcd/{frame_names[i]}.npy", points_colors)
+
+        if DEBUG_MODE and False:
+            all_data.append(new_pcd_data)
+            # coordinate = o3d.geometry.TriangleMesh.create_coordinate_frame()
+            # o3d.visualization.draw_geometries([new_pcd_data, coordinate])
+    if DEBUG_MODE and False:
+        coordinate = o3d.geometry.TriangleMesh.create_coordinate_frame()
+        o3d.visualization.draw_geometries(all_data + [coordinate])
+
+    # Store the meta_data to track
+    meta_data = {
+        "clip_path": clip_path,
+        "mean_extrinsic": mean_extrinsic.reshape(16, order="F").tolist(),
+        "min_range": min_range.tolist(),
+        "max_range": max_range.tolist(),
+    }
+
+    with open(f"{output_path}/{clip_index}/meta_data.json", "w") as f:
+        json.dump(meta_data, f)
 
 
 if __name__ == "__main__":
@@ -120,9 +163,16 @@ if __name__ == "__main__":
     # Get the clip path for all data
     clip_paths = glob.glob(f"{args.data_dir}/{args.split}/*/clips/*/*")
 
+    clip_dir = {}
+    clip_index = 0
     for clip_path in clip_paths:
-        processClip(clip_path, args.output_dir)
+        processClip(clip_path, args.output_dir, clip_index)
+        clip_dir[clip_index] = clip_path
+        clip_index += 1
 
+    # Save the clip dir
+    with open(f"{args.output_dir}/clip_dir.json", "w") as f:
+        json.dump(clip_dir, f)
     import pdb
 
     pdb.set_trace()
